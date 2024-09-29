ed106b906c7fbe26f9c544d6e74247dfed5befde
Brian Rzycki <brzycki@gmail.com>
2024-09-29 09:53:35 -0500

    git: better handling of exceptions

--- lib/git.py
+++ lib/git.py
@@ -1,17 +1,17 @@
 "Git helpers"
 
-from subprocess import run, PIPE
+from subprocess import run, CalledProcessError, PIPE
 
 
 def repo_version(repo):
     "Returns the version string for repo, unknown on errors"
-    result = run(
-        ["git", "describe", "--always", "--long", "--dirty", "--tags"],
-        cwd=str(repo),
-        stdout=PIPE,
-        check=True,
-    )
-    git_ver = result.stdout.decode("utf-8").split("\n")[0]
-    if git_ver:
-        return git_ver
-    return "unknown"
+    try:
+        result = run(
+            ["git", "describe", "--always", "--long", "--dirty", "--tags"],
+            cwd=str(repo),
+            stdout=PIPE,
+            check=True,
+        )
+        return result.stdout.decode("utf-8").split("\n")[0]
+    except CalledProcessError:
+        return "unknown"
